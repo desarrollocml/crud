c79ada16f8e29c887c013844505b7ca015a24f9e
desarrollocml <cristianmirandalamas@gmail.com>
2021-04-06 18:38:06 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const puntoenv = require('dotenv').config({ path: '.env'});
+console.log('BASE ENV  '+ process.env.DB_URL);
+
+const app = express();
+
+//mongoose.set('useUnifiedTopology', true);
+
+// connection to db
+//process.env.DB_URL    useNewUrlParser: true 
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,useUnifiedTopology:true })
+  .then(db => console.log('coneccion a base de datos'))
+  .catch(err => console.log(err));
+
+//mongoose.set('useUnifiedTopology', true);
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || '0.0.0.0');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+// routes
+app.use('/', indexRoutes);
+
+app.listen(app.get('port'),app.get('host'),() => {
+  console.log(`server on port ${app.get('port')} en la maquina ${app.get('host')}`);
+});
